6509f8e847c6e7b985d619b5172bf5252d872827
naralicecosta <naralicecosta04@gmail.com>
2023-10-08 13:52:54 -0300

    usando useStates

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,42 @@
 import { useState } from "react"
+
+interface InfoAlunoProps{
+  nome: string,
+  idade: string
+}
 export default function App(){
   const [input, setInput] = useState('') //dentro do () fica o valor inicial que queremos
   //input acessar o valor e o setInput para quando queremos trocar o valor
-  const [aluno, setAluno] = useState('')
+  //const [aluno, setAluno] = useState('')
   const [idade, setIdade] = useState('' )
+  //const [alunoIdade, setAlunoIdade] = useState('')
+
+  //criamos uma interface para não ficar fzendo vários estados
+  const[infoAluno, setInfoAluno] = useState<InfoAlunoProps>()
+
+  const [contador, setContador] = useState(0)
+
+
   function mostrarAluno(){
-    setAluno(input)
+    setInfoAluno({
+      nome: input,
+      idade: idade
+    })
+
+    //setAluno(input)
+    //setAlunoIdade(idade)
+  }
+  function adicionar(){
+    setContador(valorAtual => valorAtual+1)
+
+  }
+  function diminuir(){
+    //para não aparecer -1-2-3
+    if(contador===0){
+      return;
+    }
+    setContador(valorAtual => valorAtual-1)
+
   }
 return(
   <div>
@@ -26,8 +57,16 @@ return(
 
     <hr />
 
-    <h3>Bem vindo:{aluno}</h3>
-    <h2>sua idade é: {idade}</h2>
+    <h3>Bem vindo:{infoAluno?.nome}</h3>
+    <h4>idade:{infoAluno?.idade}</h4>
+
+    <hr />
+    <br />
+    <h1>Contador com useState</h1>
+
+    {/**vamos criar uma usestate para adc dinamicidade */}
+    {/**onclick para adiconar as ações */}
+    <button onClick={adicionar}>+</button> {contador} <button onClick={diminuir}>-</button>
     
   </div>
 
