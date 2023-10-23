50605c50860de7fc049da4d66f812be83afffe6f
naralicecosta <naralicecosta04@gmail.com>
2023-10-23 20:29:58 -0300

    adicionando arquivos de aula

--- listas/src/App.tsx
+++ listas/src/App.tsx
@@ -1,22 +1,35 @@
-import React, { useState } from 'react';
-import './App.css';
+import{ useState } from 'react';
 
-function App() {
-  const [tasks, setTask] = useState([
-    'Estudar react com typescript'
-    'comprar pao meio dia'
+export default function App() {
+  const [input, setInput] = useState("");
+  const [tasks, setTasks] = useState([
+    'Estudar react com typescript',
+    'comprar pao meio dia',
     'estudar ingles'
   ])
+  function handleRegister(){
+    
+  }
 
   return(
     <div>
       <h1>Lista de tarefas</h1>
+
+      <input placeholder="Digite o nome da tarefa..."
+      value={input}
+      onChange={(e) => setInput(e.target.value)}
+       />
+       <button
+       onClick={handleRegister}>adicionar tarefa
+       </button>
       <hr/>
-      {tasks.map(() => {})}  {/**map é uma função em js usada para percorrer uma lista */}
+      {tasks.map((item, index) => (
+        <section key={item}> {/*as keys sao utilizadas para encontrar dentro das listas e ter mais performance */}
+          <span>{item}</span>
+        </section>
+      ))}  {/**map é uma função em js usada para percorrer uma lista */}
     </div>
   )
  
   
 }
-
-export default App;
