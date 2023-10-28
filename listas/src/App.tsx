5f77468ffd47c8b3a609259d4e2ced8be8247a66
naralicecosta <naralicecosta04@gmail.com>
2023-10-28 14:24:40 -0300

    effect  de busca e executar tarefas

--- listas/src/App.tsx
+++ listas/src/App.tsx
@@ -1,7 +1,7 @@
-import{ useState, useEffect } from 'react';
-import { json } from 'stream/consumers';
+import{ useState, useEffect, useRef } from 'react';
 
 export default function App() {
+  const inputRef = useRef<HTMLInputElement>(null);
   const [input, setInput] = useState("");
   const [tasks, setTasks] = useState<string[]>([]); {/**<string[]>/ esta fazendo uma tipagem, dizendo que tasks vai ser uma string */}
   const [editTask, setEditTask] = useState({
@@ -9,11 +9,22 @@ export default function App() {
     task: ''
   })
 
+  //busca as tarefas
   useEffect(() => {
-    
+     const tarefaSalvas = localStorage.getItem("@cursoreact") // vai no local storage, busca as tarefas salvas
+      
+      if(tarefaSalvas){ // se tiver algo em tarefas salvas
+        setTasks(JSON.parse(tarefaSalvas)); //muda o usestate
+      }
 
-  },[])
+    },[])
+  
+    // executa as tarfas
+  useEffect(()=>{
+    // executar quando a usestate task mudar
+    localStorage.setItem("@cursoreact", JSON.stringify(tasks)) 
 
+  },[tasks])
 
   function handleRegister(){
     if(!input){
@@ -26,7 +37,7 @@ export default function App() {
     }
     setTasks(tarefas => [...tarefas, input])
     setInput(""); { /*para voltar o input ao status vazio */}
-    localStorage.setItem("@cursoreact", JSON.stringify([...tasks, input])); {/** ao salvar uma nova tarefa ela fica salva em local storage */}
+    //localStorage.setItem("@cursoreact", JSON.stringify([...tasks, input])); {/** ao salvar uma nova tarefa ela fica salva em local storage */}
     
   }
   function handleSaveEdit(){
@@ -41,16 +52,18 @@ export default function App() {
       task: ''
     })
 
+   
     setInput("");{/*limpar o campo */}
-    localStorage.setItem("@cursoreact", JSON.stringify(allTasks)), {/**salvando uma tarefa editada no local storage */}
+    //localStorage.setItem("@cursoreact", JSON.stringify(allTasks)), {/**salvando uma tarefa editada no local storage */}
   }
   function handleDelete(item: string){
     const removeTask = tasks.filter(task => task !== item); {/**o filter vai percorrer a lista e cm base na condição ele devolve algo */}
     setTasks(removeTask)
 
-    localStorage.setItem("@cursoreact", JSON.stringify(removeTask)) 
   }
   function handleEdit(item: string){
+    inputRef.current?.focus();
+
     {/**ao clicar em editar o conteudo vai voltar ao input para ser editado */}
     setInput(item); {/**quando clica em editar ja preenche o iput */}
     setEditTask({
@@ -66,6 +79,7 @@ export default function App() {
       <input placeholder="Digite o nome da tarefa..."
       value={input}
       onChange={ (e) => setInput(e.target.value)}
+      ref={inputRef}
        />
        <button
        onClick={handleRegister}>
